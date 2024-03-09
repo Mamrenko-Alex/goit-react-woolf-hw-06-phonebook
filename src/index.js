cc5deb9af64158b4fec289664efe68ecac9d88d1
Mamrenko-Alex <Mamrenkodev@yandex.com>
2024-03-09 12:38:25 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,12 +6,9 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    
   <React.StrictMode>
     <Provider store={store}>
-    <App />
-     </Provider>
-       </React.StrictMode>
-   
-
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
